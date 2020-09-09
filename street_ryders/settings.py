308a8a60097c41fcc55f760d04714b1d161ba84a
Monte DuMond <gdumond42@gmail.com>
2020-09-08 21:21:48 -0400

    setting adj

--- street_ryders/settings.py
+++ street_ryders/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g7g8wn(zos#(v9-g35&*+rc((+x5giu+=qs*_upc6-(avw@0g4'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['streetryders.herokuapp.com','127.0.0.1']
 
